44a5a9afbc9b408aad02e4f30e4c56565a522511
Iamtanzim <tanzim11@gmail.com>
2020-03-01 23:52:22 +0600

    Final part of Lesson-1 uploaded

---
+++ 10-TypeConversion/script.js
@@ -0,0 +1,23 @@
+// let score='101';
+// score=Number(score);
+// console.log(score+1);
+// console.log(typeof(score))
+let fruit='Apple';
+fruit=Number(fruit);
+console.log(typeof(fruit));
+
+let testnum=String(50);
+console.log(testnum,typeof(testnum));
+
+// let booleantest=true;
+// console.log(booleantest,typeof(booleantest));
+// false
+// let booleanumbertest=Boolean(0);
+// console.log(booleanumbertest,typeof(booleanumbertest));
+//true 
+// let booleanumbertest = Boolean(1);
+// console.log(booleanumbertest,typeof(booleanumbertest));
+//String 
+let booleanumbertest = Boolean(null);
+ console.log(booleanumbertest,typeof(booleanumbertest));
+ //if we leave value='' or null it will return false
---
+++ 11-LoosevsStrictComparison/script.js
@@ -0,0 +1,16 @@
+//Loose Comparisons 
+//different types can still be equal 
+let age=25;
+// console.log(age==25);
+// console.log(age=='25');
+
+// console.log(age!=25);
+// console.log(age!='25');
+// Strict Comparison 
+console.log(age===25);
+console.log(age==='25');
+
+console.log(age!==25);
+console.log(age!=='25');
+
+
